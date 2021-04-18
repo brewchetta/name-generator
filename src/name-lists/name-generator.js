da69ab5689877ec4304df4e1e9bd3b9aa7fd9416
brewchetta <chett.tiller@gmail.com>
2021-04-18 18:47:04 -0400

    names are capitalized

--- src/name-lists/name-generator.js
+++ src/name-lists/name-generator.js
@@ -1,4 +1,5 @@
 const randomNumber = (max, min = 0) => Math.floor(Math.random() * (max - min)) + min
+const capitalize = name => name[0].toUpperCase() + name.slice(1)
 
 export const generateName = (prefixes, suffixes) => {
   const suffixLength = randomNumber(3,1)
@@ -6,5 +7,5 @@ export const generateName = (prefixes, suffixes) => {
   for (let i = 0; i < suffixLength; i++) {
     name += suffixes[randomNumber(suffixes.length)]
   }
-  return name
+  return capitalize(name)
 }
