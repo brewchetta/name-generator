0f8eb12dd622f7b544019055a6efd0172517f935
brewchetta <chett.tiller@gmail.com>
2021-04-19 13:28:05 -0400

    added various sci fi animal suffixes

--- src/name-lists/sci-fi-animal.js
+++ src/name-lists/sci-fi-animal.js
@@ -1,6 +1,7 @@
-prefixes = [
+export const prefixes = [
   "bula",
-  "chara"
+  "cara",
+  "char"
   "cera",
   "din",
   "dram",
@@ -12,30 +13,47 @@ prefixes = [
   "noct",
   "octo",
   "quad",
+  "qual",
   "quasa",
   "queez",
   "sala",
+  "sla",
   "siz",
   "squi",
+  "tor",
+  "var",
+  "vlad",
+  "ver",
+  "vix",
+  "viz",
   "zoid"
 ]
 
-suffixes = [
+export const suffixes = [
+  "adon",
   "din",
+  "fid",
   "ink",
   "ite",
   "izzle",
   "mander",
+  "oid",
   "pus",
+  "sect",
+  "thrine",
   "tite",
   "toise",
+  "trax",
+  "tor",
   "val",
   "var",
   "vard",
   "vark",
+  "void",
   "zard",
   "zine",
   "zizzle",
   "zoid",
   "zonk",
+  "zord",
 ]
